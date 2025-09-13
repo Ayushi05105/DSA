d90f63f804ff642d0e8166e1b1ccafe864e73036
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-09-13 23:15:20 +0530

    updated code

---
+++ Step1/Lec1/input.java
@@ -0,0 +1,14 @@
+package Step1.Lec1;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter input");
+        int n = sc.nextInt();
+        System.out.println(n);
+        sc.close();
+    }
+    
+}
