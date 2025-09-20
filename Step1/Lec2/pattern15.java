0e706b5427de6c4675cae64cf87ef4e42706a342
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-09-20 19:56:44 +0530

    updated code

---
+++ Step1/Lec2/pattern15.java
@@ -0,0 +1,18 @@
+package Step1.Lec2;
+
+public class pattern15 {
+    static void pattern(int n){
+        int N=1;
+        for(int i =0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                System.out.print(N + " ");
+                N=N+1;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    
+}
