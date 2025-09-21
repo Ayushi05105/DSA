1d27171ff1de99d4c3cd8b1ac8826dbe601d7387
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-09-21 12:12:02 +0530

    updated code

---
+++ Step1/Lec2/pattern17.java
@@ -0,0 +1,17 @@
+package Step1.Lec2;
+
+public class pattern17 {
+    static void pattern(int n){
+        for(int i =0;i<n;i++){
+            for(char ch ='A';ch<='A'+i;ch++){
+                System.out.print(ch+"");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        pattern(5);
+    }
+    
+}
