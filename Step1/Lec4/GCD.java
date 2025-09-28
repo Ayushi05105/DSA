68a97b1bdc9b1892d8f34e8727a01349195c0942
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-09-28 19:33:45 +0530

    updated code

---
+++ Step1/Lec4/GCD.java
@@ -0,0 +1,20 @@
+package Step1.Lec4;
+
+public class GCD {
+    static int findGcd(int n1,int n2){
+        int gcd =1;
+        for(int i=1;i<Math.min(n1,n2);i++){
+            if(n1 % i==0 && n2 % i ==0){
+                gcd =i;
+            }
+        }
+        return gcd;
+    }
+    public static void main(String[] args) {
+        int n1=20;
+        int n2=5;
+        int gcd =findGcd(n1, n2);
+        System.out.println("GCd of " + n1 +" and " + n2 + " is " + gcd);
+    }
+    
+}
