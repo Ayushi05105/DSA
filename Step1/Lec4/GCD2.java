47d3c340e53ca4b0ad1b6401329118df5e517bdb
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-09-29 19:09:31 +0530

    updated code

---
+++ Step1/Lec4/GCD2.java
@@ -0,0 +1,27 @@
+package Step1.Lec4;
+// optimal approach
+public class GCD2 {
+
+    static void gcd(int a,int b){
+        while(a>0 && b>0){
+            if(a>b){
+                a = a%b;
+            }
+            else{
+                b = b%a;
+            }
+        }
+            if(a==0){
+                System.out.println(b + " ");
+            }
+            else{
+                System.out.println(a +" ");
+            }
+        }
+    public static void main(String[] args) {
+        int a =52;
+        int b = 10;
+        gcd(a, b);
+    }
+    
+}
