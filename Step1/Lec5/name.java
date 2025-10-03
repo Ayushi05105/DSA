10505ec7865493b2ecd3094d4f71ce90bd904872
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-10-03 21:50:42 +0530

    updated code

---
+++ Step1/Lec5/name.java
@@ -0,0 +1,14 @@
+package Step1.Lec5;
+
+public class name {
+    static void func(int i,int n){
+        if(i>n) return;
+        System.out.println("Ayushi ");
+        func(i+1,n);
+    }
+    public static void main(String[] args) {
+        int n =5;
+        func(1, n);
+    }
+    
+}
