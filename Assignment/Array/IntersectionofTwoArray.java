aa86a3a603ce91fb3d23da85179ca971127051dd
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-12-23 19:24:11 +0530

    updated code

--- Assignment/Array/IntersectionofTwoArray.java
+++ Assignment/Array/IntersectionofTwoArray.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class IntersectionofTwoArray {
 
 
+    //if duplicates not allow
+
     public static void intersection_array(int[] arr1, int[] arr2){
         Arrays.sort(arr1);
         Arrays.sort(arr2);
@@ -28,6 +30,36 @@ public class IntersectionofTwoArray {
     }
 
     public static int[] intersection(int[] arr1, int[] arr2){
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        int i =0;
+        int j = 0;
+        List<Integer> list = new ArrayList<>();
+        while(i< arr1.length && j < arr2.length){
+            if(arr1[i] == arr2[j]){
+                if(list.isEmpty() || list.get(list.size()-1) != arr1[i]){
+                     list.add(arr1[i]);
+                }
+                i++;
+                j++;
+            }
+            else if(arr1[i] > arr2[j]){
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        int[] result = new int[list.size()];
+        for(int k =0;k<list.size();k++){
+            result[k] = list.get(k);
+        }
+        return result;
+    }
+
+    // if duplicates allows
+
+    public static int[] intersection1(int[] arr1, int[] arr2){
         Arrays.sort(arr1);
         Arrays.sort(arr2);
         int i =0;
