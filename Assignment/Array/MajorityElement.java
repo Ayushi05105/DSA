a4ec870c08f462c0da9fe3d21d16b4db5c91c611
Ayushi05105 <ayushijaiswal8720@gmail.com>
2025-12-25 15:57:32 +0530

    updated code

---
+++ Assignment/Array/MajorityElement.java
@@ -0,0 +1,36 @@
+package Assignment.Array;
+
+public class MajorityElement {
+
+
+     public static int majorityElement(int[] nums) {
+       int cnt =0;
+       int el =0 ;
+       for(int i =0;i<nums.length;i++){
+        if(cnt == 0){
+            cnt = 1;
+            el=nums[i];
+        }
+        else if(cnt ==nums[i]){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+       }
+       int cnt1 =0;
+       for(int i =0;i<nums.length;i++){
+        if(nums[i] == el) cnt1++;
+       }
+        if(cnt1 > nums.length/2){
+             return el;
+        }
+       return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,5,2,3,5,6,5,5,5,5,5};
+        System.out.println(majorityElement(nums));
+    }
+    
+}
