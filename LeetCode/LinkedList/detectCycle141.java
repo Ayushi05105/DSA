5494768522528bc022d07b43b941f60c520e5c9b
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-04 00:05:21 +0530

    updated code

---
+++ LeetCode/LinkedList/detectCycle141.java
@@ -0,0 +1,26 @@
+package LeetCode.LinkedList;
+
+public class detectCycle141 {
+    public static class Node{
+        Node next ;
+        int val;
+        public  Node(int val){
+            this.val = val;
+        }
+    }
+    public static boolean cycle(Node head){
+        if(head == null) return false;
+        if(head.next == null) return false;
+        Node slow = head;
+        Node fast = head;
+        while(fast != null){
+            if(slow == null) return false;
+            slow = slow.next;
+            if(fast == null) return false;
+            fast = fast.next.next;
+            if(fast == slow ) return true;
+        }
+        return false;
+    }
+    
+}
