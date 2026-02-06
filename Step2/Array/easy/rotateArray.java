48e4063d067c1fda33e2c7f323a8a67fd02bbf7a
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-06 21:44:14 +0530

    updated code

---
+++ Step2/Array/easy/rotateArray.java
@@ -0,0 +1,24 @@
+package Step2.Array.easy;
+
+public class rotateArray {
+    public static void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void reverse(int[] arr,int i ,int j){
+        while(i<j){
+            swap(arr,i,j);
+            i++;
+            j--;
+        }
+    }
+    public static void rotate(int[] arr,int k){
+        int n = arr.length;
+        k = k%n;
+        reverse(arr, 0, n-k-1);
+        reverse(arr, n-k, n-1);
+        reverse(arr, 0, n-1);
+    }
+    
+}
