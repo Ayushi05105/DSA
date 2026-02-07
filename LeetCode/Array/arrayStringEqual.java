eded22549aecbebcecdc5d2799feb95a9d2959c3
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-08 01:00:34 +0530

    updated code

---
+++ LeetCode/Array/arrayStringEqual.java
@@ -0,0 +1,16 @@
+package LeetCode.Array;
+
+public class arrayStringEqual {
+      public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String s1 ="";
+        String s2 ="";
+        for(String i :word1){
+            s1+=i;
+        }
+        for(String i :word2){
+            s2+=i;
+        }
+        return s1.equals(s2);
+    }
+   
+}
