713be20e0f35a10c43e3bc67d0c3c381af1f2624
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-09 01:05:12 +0530

    updated code

---
+++ Step2/Array/easy/union.java
@@ -0,0 +1,56 @@
+package Step2.Array.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class union {
+
+    public static ArrayList<Integer> unionSortedArray(int[] arr1,int[] arr2){
+        ArrayList<Integer> union = new ArrayList<>();
+        int n = arr1.length;
+        int m = arr2.length;
+        int i =0,j=0;
+        while(i<n && j<m){
+            if(arr1[i] < arr2[j]){
+                if(union.isEmpty() || union.get(union.size()-1)!=arr1[i]){
+                    union.add(arr1[i]);
+                }
+            i++;
+            }
+            else if(arr1[i] > arr2[j]){
+                if(union.isEmpty() || union.get(union.size()-1)!=arr2[j]){
+                    union.add(arr2[j]);
+                }
+            j++;
+            }
+            else{
+            if(union.isEmpty() || union.get(union.size()-1)!=arr1[i]){
+                    union.add(arr1[i]);
+                }
+            i++;
+            j++;
+            }
+        }
+        while(i<n){
+            if(union.isEmpty() || union.get(union.size()-1)!=arr1[i]){
+                    union.add(arr1[i]);
+                }
+            i++;
+        }
+        while(j<m){
+            if(union.isEmpty() || union.get(union.size()-1)!=arr2[j]){
+                    union.add(arr2[j]);
+                }
+            j++;
+        }
+        return union;
+    }
+    public static void main(String[] args) {
+        int[] arr1 ={1,2,3,4,7,5};
+        int[] arr2 = {3,4,5,6,7,0};
+        union obj = new union();
+        List<Integer> res = union.unionSortedArray(arr1,arr2);
+        for(int val : res) System.out.print(val +" ");
+    }
+    
+}
