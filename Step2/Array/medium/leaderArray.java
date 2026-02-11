d4d3f85de9a762276fd40f43b85ba8449f681db1
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-11 11:50:51 +0530

    updated code

--- Step2/Array/medium/leaderArray.java
+++ Step2/Array/medium/leaderArray.java
@@ -1,6 +1,7 @@
 package Step2.Array.medium;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class leaderArray {
 
@@ -21,9 +22,28 @@ public class leaderArray {
         return ans;
     }
 
+    public static ArrayList<Integer> leader1(int[] arr){
+        int n = arr.length;
+        ArrayList<Integer> ans = new ArrayList<>();
+        if(arr.length ==0){
+            return ans;
+        }
+        int max = arr[n-1];
+        ans.add(arr[n-1]);
+        for(int i =n-2;i>=0;i--){
+            if(arr[i] > max){
+                ans.add(arr[i]);
+                max = arr[i];
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = {-3,4,5,1,-4,-5};
         System.out.println(leaders(arr));
+        System.out.println(leader1(arr));
     }
     
 }
