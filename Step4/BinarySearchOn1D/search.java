921fa81d3514519e61d5d7769efc0f3509d07b57
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-13 22:29:50 +0530

    updated code

--- Step4/BinarySearchOn1D/search.java
+++ Step4/BinarySearchOn1D/search.java
@@ -1,10 +1,24 @@
 package Step4.BinarySearchOn1D;
 
+// TC->O(N)
 public class search {
     public static int search1(int[] nums, int target) {
         int n = nums.length;
         for(int i =0;i<n;i++){
             if(nums[i]==target) return i;
+        }
+        return -1;
+    }
+  
+    // TC->O(logN)
+    public int search2(int[] nums, int target) {
+        int n = nums.length;
+        int low =0,high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target) return mid;
+            else if(target > nums[mid]) low = mid+1;
+            else high = mid-1;
         }
         return -1;
     }
