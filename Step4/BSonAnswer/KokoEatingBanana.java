4eb6edc898a50ed2579f8466b252b262ea6bed42
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-16 00:16:31 +0530

    updated code

---
+++ Step4/BSonAnswer/KokoEatingBanana.java
@@ -0,0 +1,40 @@
+package Step4.BSonAnswer;
+
+public class KokoEatingBanana {
+    
+    public static long totalHours(int[] piles,int speed){
+        long total = 0;
+        for(int bananas :piles){
+            total += (bananas + speed -1)/speed;
+
+        }
+        return total;
+    }
+    public static int minEatingSpeed(int[] piles, int h) {
+        int maxPile = piles[0];
+        for(int i =0;i<piles.length;i++){
+            if(piles[i]>maxPile){
+                maxPile = piles[i];
+            }
+        }
+        int low =1;
+        int high = maxPile;
+        int ans = maxPile;
+        while(low<=high){
+            int mid=low + (high - low)/2;
+            long total = totalHours(piles,mid);
+            if(total<=h){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] piles ={3,6,7,11};
+        int h = 18;
+        System.out.println(minEatingSpeed(piles, h));
+    }
+}
