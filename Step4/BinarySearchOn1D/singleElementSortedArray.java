047475ca6656a63c680eeeb5328a73467be9420c
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-15 18:28:10 +0530

    updated code

---
+++ Step4/BinarySearchOn1D/singleElementSortedArray.java
@@ -0,0 +1,19 @@
+package Step4.BinarySearchOn1D;
+
+public class singleElementSortedArray {
+    public int singleNonDuplicate(int[] arr) {
+        int n = arr.length;
+        if(n==1) return arr[0];
+        for(int i =0;i<n;i++){
+            if(i==0){
+                if(arr[i]!=arr[i+1]) return arr[i];
+            }
+            else if(i==n-1){
+                if(arr[i]!=arr[i-1]) return arr[i];
+            }else{
+                if(arr[i]!=arr[i+1] && arr[i]!=arr[i-1]) return arr[i];
+            }
+        }
+        return -1;
+    }
+}
---
+++ Step4/BinarySearchOn2D/sqrt.java
@@ -0,0 +1,22 @@
+package Step4.BinarySearchOn2D;
+
+public class sqrt {
+    public static int floorSqrt(int n) {
+      int ans =0;
+      for(int i =1;i<=n;i++){
+        if((long)(i)*i<=n){
+            ans = i;
+        }
+        else{
+            break;
+        }
+      }
+      return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 50;
+        System.out.println(floorSqrt(n));
+    }
+}
+    
