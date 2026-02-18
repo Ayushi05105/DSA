35cc3ab0a5a0cd25a239b2d064d6f09b76595261
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-18 09:35:29 +0530

    updated code

---
+++ Step8/basic/hamming.java
@@ -0,0 +1,22 @@
+package Step8.basic;
+
+public class hamming {
+    public static int hammingWeight(int n) {
+      String binary = "";
+      while(n>0){
+        int rem = n%2;
+        binary = rem + binary;
+        n = n/2;
+      }  
+      int count =0;
+      for(int i =0;i<binary.length();i++){
+        if(binary.charAt(i)=='1') count++;
+      }
+      return count;
+    }
+    public static void main(String[] args) {
+        int n = 11;
+        System.out.println(hammingWeight(n));
+    }
+    
+}
