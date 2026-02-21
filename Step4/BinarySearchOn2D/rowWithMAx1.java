f0744228f5bf35b6958f6c3d757037a3db6966be
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-21 15:01:27 +0530

    updated code

--- Step4/BinarySearchOn2D/rowWithMAx1.java
+++ Step4/BinarySearchOn2D/rowWithMAx1.java
@@ -1,5 +1,7 @@
 package Step4.BinarySearchOn2D;
 
+import Step4.BinarySearchOn1D.search;
+
 public class rowWithMAx1 {
     public static int rowWithMax1s(int[][] matrix,int n ,int m){
         int cnt =0;
@@ -16,6 +18,36 @@ public class rowWithMAx1 {
         }
         return idx;
     }
+
+    //optimal approach
+    public static int lowerBound(int[] arr,int n,int x){
+        int low =0;
+        int high = n-1;
+        int ans =n;
+        while(low<=high){
+            int mid =(low+high)/2;
+            if(arr[mid]>=x){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+
+    public static int rowWithMax(int[][] matrix,int n ,int m){
+        int cnt =0;
+        int idx =-1;
+        for(int i =0;i<n;i++){
+            int cntOne = m -lowerBound(matrix[i], m, 1);
+            if(cntOne>cnt){
+                cnt = cntOne;
+                idx =i;
+            }
+        }
+        return idx;
+    }
     public static void main(String[] args) {
         int n =3;
         int m = 3;
@@ -23,5 +55,6 @@ public class rowWithMAx1 {
                         {0,0,1},
                          {0,0,0}};
         System.out.println(rowWithMax1s(matrix, n, m));
+        System.out.println(rowWithMax(matrix, n, m));
     }
 }
