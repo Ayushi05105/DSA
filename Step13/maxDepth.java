ba986b06411339739358e003935a8305b7ff296f
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-02-26 22:32:23 +0530

    updated code

---
+++ Step13/maxDepth.java
@@ -0,0 +1,30 @@
+package Step13;
+
+public class maxDepth {
+    public static class Node{
+        Node root;
+        Node left;
+        Node right;
+        int val;
+        Node(int val){
+            this.val = val;
+         
+        }
+    }
+
+    public static int MaxDepth(Node root){
+        if(root == null) return 0;
+        int lh = MaxDepth(root.left);
+        int rh = MaxDepth(root.right);
+        return 1+Math.max(lh,rh);
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        Node a = new Node(0);
+        Node b = new Node(2);
+        root.left = a;
+        root.right = b;
+        System.out.println(MaxDepth(root));
+    }
+}
