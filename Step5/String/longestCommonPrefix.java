8f299d144448e6b521bfb1dd1f90bd4c228edd27
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-03-15 23:08:23 +0530

    updated code

---
+++ Step5/String/longestCommonPrefix.java
@@ -0,0 +1,19 @@
+package Step5.String;
+
+public class longestCommonPrefix {
+    public static String longestPrefix(String[] s){
+        if(s == null || s.length==0) return "";
+        String prefix = s[0];
+        for(int i =1;i<s.length;i++){
+            while(!s[i].startsWith(prefix)){
+                prefix = prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+    public static void main(String[] args) {
+      String[] s ={"flower","flow","flight"}; 
+      System.out.println(longestPrefix(s)); 
+    }
+}
