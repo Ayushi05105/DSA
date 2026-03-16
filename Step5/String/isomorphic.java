73c375ca10c550f574a8b6783decabab0659d0a5
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-03-16 22:52:25 +0530

    updated code

--- Step5/String/isomorphic.java
+++ Step5/String/isomorphic.java
@@ -18,11 +18,26 @@ public class isomorphic {
         }
         return true;
     }
+    public static boolean isomorphicString2(String s,String t){
+        int[] s1 = new int[256];
+        int[] t1 = new int[256];
+        for(int i =0;i<s.length();i++){
+            if(s1[s.charAt(i)] != t1[t.charAt(i)]){
+                return false;
+            }
+            s1[s.charAt(i)] = i+1;
+            t1[t.charAt(i)] = i+1;
+            
+        }
+        return true;
+    }
     public static void main(String[] args) {
         String s ="paper";
         String t = "title";
         boolean result = isomorphicString(s, t);
         System.out.println(result);
+        boolean res1 = isomorphicString(s, t);
+        System.out.println(res1);
     }
     
 }
