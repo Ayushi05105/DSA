76e3c3f41d46108524028807c9d559482a0032ac
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-03-21 00:20:51 +0530

    updated code

--- Step6/Recursion/powerSet.java
+++ Step6/Recursion/powerSet.java
@@ -13,8 +13,24 @@ public class powerSet {
             System.out.print(sub+" ");
         }
     }
+
+    public static void helper(String s,int idx,String curr){
+        if(idx != s.length()){
+            System.out.println(curr+" ");
+            return;
+        }
+            helper(s, idx+1, curr+s.charAt(idx));
+            helper(s, idx+1, curr);
+       
+    }
+
+    public static void PowerSet1(String s){
+        helper(s, 0, "");
+    }
     public static void main(String[] args) {
         String s ="abc";
         PowerSet(s);
+        PowerSet1(s);
+        
     }
 }
