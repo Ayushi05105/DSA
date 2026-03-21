8e590c72dae3aae7753c1c8e68affeeb98b4e798
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-03-21 17:29:06 +0530

    updated code

--- Step6/Recursion/kthSubsequence.java
+++ Step6/Recursion/kthSubsequence.java
@@ -13,10 +13,25 @@ public class kthSubsequence {
         int r = printSubsequence(idx+1, s, sum, arr, n);
         return l+r;
     }
+
+    public static boolean subsequence(int idx,int n ,int[] arr,int k){
+        if(k==0) return true;
+        if(k<0) return false;
+        if(idx==n){
+            return k==0;
+        }
+        return subsequence(idx+1, n, arr, k-arr[idx]) || subsequence(+1, n, arr, k);
+    }
+    public static boolean kthSubsequenceSum(int[] arr,int target){
+        int n = arr.length;
+        return subsequence(0, n, arr, target);
+    }
     public static void main(String[] args) {
         int[] arr ={1,2,1};
         int n = 3;
         int sum = 2;
+        int target = 2;
         System.out.println(printSubsequence(0, 0, sum, arr, n));
+        System.out.println(kthSubsequenceSum(arr, target));
     }
 }
