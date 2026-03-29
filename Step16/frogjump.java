cdf3e98e7407add5224d29c18db60be1247af6aa
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-03-29 14:36:26 +0530

    updated code

--- Step16/frogjump.java
+++ Step16/frogjump.java
@@ -37,10 +37,45 @@ public class frogjump {
         }
         return dp[n-1];
     }
+
+    public static int Tabulation(int n, int[] arr){
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        for(int i =1;i<n;i++){
+            int left = dp[i-1] + Math.abs(arr[i]-arr[i-1]);
+            int right = Integer.MAX_VALUE;
+            if(i>1){
+                right = dp[i-2] + Math.abs(arr[i]-arr[i-2]);
+            }
+            dp[i] = Math.min(left,right);
+        }
+        return dp[n-1];
+    }
+
+    public static int spaceOptimisation(int n ,int[] arr){
+        int prev = 0;
+        int prev2 =0;
+        for(int i =1;i<n;i++){
+            int left = prev + Math.abs(arr[i]-arr[i-1]);
+            int right = Integer.MAX_VALUE;
+            if(i>1){
+                right = prev2 + Math.abs(arr[i]-arr[i-2]);
+            }
+            int curr = Math.min(left,right);
+            prev2 = prev;
+            prev = curr;
+
+        }
+        return prev;
+    }
     public static void main(String[] args) {
         int[] arr={30,10,60,10,60,50};
+        int n = arr.length;
         System.out.println(FrogJump(arr.length, arr));
         System.out.println(FrogJump2(arr.length, arr));
+        System.out.println(Tabulation(n, arr));
+        System.out.println(spaceOptimisation(n, arr));
 
     }
 }
