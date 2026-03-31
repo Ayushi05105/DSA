bfba4f21f968168b0f557d1bf7d3c94b82575f73
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-03-31 19:08:49 +0530

    updated code

--- Step16/twoDimention/triangle.java
+++ Step16/twoDimention/triangle.java
@@ -37,11 +37,34 @@ public class triangle {
         return helper2(0, 0,n, grid,dp);
     }
 
+
+    //TABULATION
+    public static int tabulation(int[][] grid ){
+        int n = grid.length;
+        int[][] dp = new int[n][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        
+        for(int  j = 0;j<n;j++){
+            dp[n-1][j] = grid[n-1][j];
+        }
+        for(int i = n-2;i>=0 ;i--){
+            for(int j = i;j>=0;j--){
+                int down = grid[i][j] + dp[i+1][j];
+                int diagonal = grid[i][j] + dp[i+1][j+1];
+                dp[i][j] = Math.min(down,diagonal);
+            }
+        }
+        return dp[0][0];
+    }
+
     public static void main(String[] args) {
         
         int[][] grid = {{2},{3,4},{6,5,7},{4,1,8,3}};
         System.out.println(recursion(grid));
         System.out.println(memoization(grid));
+        System.out.println(tabulation(grid));
     }
     
 }
