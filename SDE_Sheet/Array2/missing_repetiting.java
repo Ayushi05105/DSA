9816fb5ad8642d7bdbc3eabdfab01d7658a9f047
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-06-08 23:44:18 +0530

    updated code

--- SDE_Sheet/Array2/missing_repetiting.java
+++ SDE_Sheet/Array2/missing_repetiting.java
@@ -6,25 +6,48 @@ public class missing_repetiting {
     public static int[] findMissingRepeating(int[] nums){
         int n = nums.length;
         int[] freq = new int[n+1];
-        Arrays.fill(freq,0);
+       
 
         for(int i =0;i<n;i++){
             freq[nums[i]]++;
         }
         int repeating = -1;
         int missing = 1;
-        for(int i =0;i<n;i++){
-            if(nums[i]==2) repeating = i;
-            else if(nums[i]==0) missing = i;
+        for(int i =1;i<=n;i++){
+            if(freq[i]==2) repeating = i;
+            else if(freq[i]==0) missing = i;
             if(repeating !=-1 && missing !=-1){
                 break;
             }
         }
         return new int[]{repeating,missing};
     }
+
+    public static int[] findMissingRepeating2(int[] nums){
+        int n = nums.length;
+        long sum = (n*(n+1))/2;
+        long sumSqr = (n*(n+1)*(2*n+1))/6;
+        long s =0;
+        long s2 =0;
+        for(int i =0;i<n;i++){
+            s +=nums[i];
+            s2 +=(long) nums[i]*nums[i]; 
+        }
+        long val1 = s-sum;
+        long val2  = (s2-sumSqr)/val1;
+       // val2 = val2/val1;
+        long x = (val1+val2)/2;
+        long y = x-val1;
+
+        return new int[]{(int)x,(int)y};
+
+    }
     public static void main(String[] args) {
         int[] nums={1,2,3,6,7,5,7};
-        System.out.println(findMissingRepeating(nums));
+        int[] ans = findMissingRepeating(nums);
+        int[] ans2 = findMissingRepeating2(nums);
+        System.out.println(Arrays.toString(ans));
+        System.out.println(Arrays.toString(ans2));
     }
     
 }
