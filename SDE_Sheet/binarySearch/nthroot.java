b32c56807e50e57372c9a8f7ecfdc02a918b97e9
Ayushi05105 <ayushijaiswal8720@gmail.com>
2026-07-13 22:38:19 +0530

    updated code

--- SDE_Sheet/binarySearch/nthroot.java
+++ SDE_Sheet/binarySearch/nthroot.java
@@ -11,10 +11,31 @@ public class nthroot {
         }
         return -1;
     }
+
+    public static int root2(int n ,int m){
+        int low =1;
+        int high = m;
+
+        while(low<=high){
+            int mid = (low+high)/2;
+            long ans =1;
+            for(int i =0;i<n;i++){
+             ans *= mid;
+             if(ans >m) break;
+           }
+             if(ans == m) return mid;
+
+             if(ans<m) low = mid+1;
+             else high = mid-1;
+           }
+        return -1;
+        
+    }
     public static void main(String[] args) {
         int n = 3;
         int m = 27;
         System.out.println(root(n, m));
+        System.out.println(root2(n, m));
     }
     
 }
